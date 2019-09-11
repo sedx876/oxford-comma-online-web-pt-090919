eb9b69e7438606ad2a752b0a4c498a9e3ed5e155
Sharon Watkins <swatkins876@gmail.com>
2019-09-11 13:16:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,9 @@ def oxford_comma(array)
  if array.length == 1 
    return array.join
  elsif array.length == 2 
-   return array.join("and")
+   return array.join(" and ")
  elsif array.length >= 3
     array[-1] = "and #{array[-1]}"
-    return array.join(",")
+    return array.join(" , ")
 end
 end 

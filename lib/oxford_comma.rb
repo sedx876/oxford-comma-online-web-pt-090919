8e41749cfe362370154586be6d8608c410eb0782
Sharon Watkins <swatkins876@gmail.com>
2019-09-11 13:18:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,6 @@ def oxford_comma(array)
    return array.join(" and ")
  elsif array.length >= 3
     array[-1] = " and #{array[-1]}"
-    return array.join(" , ")
+    return array.join(", ")
 end
 end 

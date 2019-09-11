bbbe93b6ddb6df5d76060a1ecf4f02aaff58b088
Sharon Watkins <swatkins876@gmail.com>
2019-09-11 13:16:17 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,5 +2,8 @@ def oxford_comma(array)
  if array.length == 1 
    return array.join
  elsif array.length == 2 
-   return array.join("and") 
+   return array.join("and")
+ elsif array.length >= 3
+    array[-1] = "and #{array[-1]}"
+    return array.join(",")
 end
